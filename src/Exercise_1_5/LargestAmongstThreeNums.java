1e535d31e6d60c767fe0f003092c5daf7181150b
oluwatimilehin awoniyi <awoniyitimi@gmail.com>
2024-09-19 04:23:39 +0100

    a program that finds the largest among three numbers given from the cli, using if-else

---
+++ src/Exercise_1_5/LargestAmongstThreeNums.java
@@ -0,0 +1,21 @@
+package Exercise_1_5;
+
+// Write a program that finds the largest among three numbers given from the cli, using if-else.
+
+public class LargestAmongstThreeNums {
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Three numbers have to be provided");
+            return;
+        }
+
+        int num1 = Integer.parseInt(args[0]), num2 = Integer.parseInt(args[1]), num3 = Integer.parseInt(args[2]);
+
+        if (num1 > num2 && num1 > num3)
+            System.out.println(num1 + " is greater than " + num2 + " and " + num3);
+        else if (num2 > num1 && num2 > num3) {
+            System.out.println(num2 + " is greater than " + num1 + " and " + num3);
+        } else
+            System.out.println(num3 + " is greater than " + num1 + " and " + num2);
+    }
+}
